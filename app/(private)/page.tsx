31112e558f81d3e7bfc9473e2dd4cb446212cead
Clément <33451311+clmntgbr@users.noreply.github.com>
2026-02-02 22:52:56 +0100

    Integrate endpoint context into private page: add useEndpoint hook to fetch and display endpoints in JSON format, enhancing the user interface for endpoint management.

--- app/(private)/page.tsx
+++ app/(private)/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { CreateEndpoint } from "@/components/endpoint/create-endpoint";
+import { useEndpoint } from "@/lib/endpoint/context";
 
 export default function Page() {
+  const { endpoints } = useEndpoint();
   return (
     <>
+      <pre>{JSON.stringify(endpoints, null, 2)}</pre>
       <CreateEndpoint />
     </>
   );
