626f4a2d74891b34572527659d32e42ced10ead9
Clément <33451311+clmntgbr@users.noreply.github.com>
2026-02-06 20:13:45 +0100

    Update layout of private page: enhance styling by adding container and padding classes to the main div for improved responsiveness and alignment.

--- app/(private)/page.tsx
+++ app/(private)/page.tsx
@@ -5,7 +5,7 @@ import { CreateWorkflow } from "@/components/workflow/create-workflow";
 export default function Page() {
   return (
     <>
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center container max-w-7xl px-4 mx-auto">
         <CreateWorkflow />
       </div>
     </>
